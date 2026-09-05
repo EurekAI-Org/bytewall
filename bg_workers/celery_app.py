171882d224a6c2459be88226b0767744be66d6f7
lucifermorningstar1305 <adityam.ghosh@gmail.com>
2026-09-06 02:14:37 +1200

    Updated license header

--- bg_workers/celery_app.py
+++ bg_workers/celery_app.py
@@ -1,11 +1,7 @@
 # LICENSE HEADER MANAGED BY add-license-header
 #
-# Copyright (c) 2026 Adityam Ghosh (@lucifermorningstar1305). All rights reserved.
-#
-# This software is proprietary and confidential.
-# Use, reproduction, modification, or distribution is permitted only
-# as expressly authorized under a written licence or agreement
-# with the copyright holder.
+# Copyright (c) 2026 EurekAI Pvt. Ltd.
+# SPDX-License-Identifier: MIT
 #
 
 from celery import Celery
